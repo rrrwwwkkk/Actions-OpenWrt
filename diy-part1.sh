17f31ba8ba4d85c9afcc55c30021d1ecb7faf02e
hellorwk <62873920+rrrwwwkkk@users.noreply.github.com>
2022-10-02 23:11:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git rosy https://github.com/rosywrt/luci-theme-rosy.git" >> feeds.conf.default
