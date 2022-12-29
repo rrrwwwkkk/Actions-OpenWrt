abb7b299536a28bdb3d9ec30bf122284e7d831ac
hellorwk <62873920+rrrwwwkkk@users.noreply.github.com>
2022-12-29 11:32:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
-echo "src-git rosy https://github.com/rosywrt/luci-theme-rosy.git" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >>feeds.conf.default
+# echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+# echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+# echo "src-git rosy https://github.com/rosywrt/luci-theme-rosy.git" >> feeds.conf.default
